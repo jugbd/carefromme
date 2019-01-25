cf6abff12683d57bb93d4d0220fa0083b31e3610
Bazlur Rahman <anmbrr.bit0112@gmail.com>
2019-01-26 04:18:41 +0600

    Initial commit

---
+++ src/main/java/com/jugbd/careforme/CareformeApplication.java
@@ -0,0 +1,14 @@
+package com.jugbd.careforme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareformeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CareformeApplication.class, args);
+    }
+
+}
+
